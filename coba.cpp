a7c0588c8b39f5af674ce31e8a25c9237ecee31f
Muhammad Falah A <mfalah16@gmail.com>
2023-08-09 17:23:38 +0700

    gatau mau isi apa:v

---
+++ coba.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+   string nama; 
+   cout << "Masukkan nama anda : ";
+   getline(cin, nama); 
+
+   // string ngambil variabel dan terpisah dari spasi
+   cout << "Nama anda adalah : " << nama;  
+}
+
